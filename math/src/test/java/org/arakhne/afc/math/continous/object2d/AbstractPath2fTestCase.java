a6a1ccb404b90b0b4cb34f1330e14fb16339741c
sgalland <stephane.galland@utbm.fr>
2013-05-03 15:54:10 +0200

    * Change license version from LGPL 2.1 to LGPL 3.0

--- math/src/test/java/org/arakhne/afc/math/continous/object2d/AbstractPath2fTestCase.java
+++ math/src/test/java/org/arakhne/afc/math/continous/object2d/AbstractPath2fTestCase.java
@@ -7,7 +7,7 @@
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -201,6 +201,8 @@ public abstract class AbstractPath2fTestCase extends AbstractShape2fTestCase<Pat
 	@Override
 	public abstract void testToBoundingBox();
 	
+	/**
+	 */
 	public abstract void testToBoundingBoxWithCtrlPoints();
 
 	/**
--- ui/ui-base/src/main/java/org/arakhne/afc/ui/event/PointerEvent.java
+++ ui/ui-base/src/main/java/org/arakhne/afc/ui/event/PointerEvent.java
@@ -6,7 +6,7 @@
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -158,7 +158,6 @@ public interface PointerEvent extends InputEvent {
 	 * @return <code>true</code> if the pointer area intersect
 	 * the given shape; <code>false</code> otherwise.
 	 * @see #getToolArea(int)
-	 * @see #inside(Shape2f)
 	 */
 	public boolean intersects(Shape2f s);
 	
