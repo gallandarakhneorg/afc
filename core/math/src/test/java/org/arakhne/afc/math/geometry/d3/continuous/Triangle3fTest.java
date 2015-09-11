fd936feb19aa051720394d3cad2d947d121a5c92
hjaffali <hjaffali@pc-set-125.utbm.local>
2015-09-11 15:11:33 +0200

    push

---
+++ core/math/src/test/java/org/arakhne/afc/math/geometry/d3/continuous/Triangle3fTest.java
@@ -0,0 +1,332 @@
+/* 
+ * $Id$
+ * 
+ * Copyright (C) 2010-2012 Stephane GALLAND.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * This program is free software; you can redistribute it and/or modify
+ */
+
+/**
+ * @author $Author: hjaffali$
+ * @version $FullVersion$
+ * @mavengroupid $GroupId$
+ * @mavenartifactid $ArtifactId$
+ */
+package org.arakhne.afc.math.geometry.d3.continuous;
+
+import static org.arakhne.afc.math.MathConstants.PI;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+import org.arakhne.afc.math.AbstractMathTestCase;
+import org.arakhne.afc.math.geometry.coordinatesystem.CoordinateSystem3D;
+import org.arakhne.afc.math.geometry.d2.continuous.Vector2f;
+import org.arakhne.afc.math.geometry.d3.Vector3D;
+import org.arakhne.afc.math.matrix.Matrix3f;
+import org.junit.Test;
+
+public class Triangle3fTest extends AbstractMathTestCase {
+
+	@Test
+	public void mollerAlgorithmAxisTestX01() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void mollerAlgorithmAxisTestX02() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void mollerAlgorithmAxisTestY02() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void mollerAlgorithmAxisTestY01() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void mollerAlgorithmAxisTestZ12() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void mollerAlgorithmAxisTestZ0() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void mollerAlgorithmPlaneBoxOverlap() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void intersectsTriangleAlignedBox() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void toPlane() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Test
+	public void computeClosestPointTrianglePoint() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Test
+	public void distanceSquaredTriangleSegment() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Test
+	public void intersectsTriangleCapsule() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Test
+	public void intersectsCoplanarTriangleTriangle() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Test
+	public void intersectsTriangleSphere() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Test
+	public void intersectsTriangleOrientedBox() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void intersectsTriangleSegment() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	
+	@Test
+	public void getTriangleSegmentIntersectionFactorWithJimenezAlgorithm() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getTriangleSegmentIntersectionFactorWithBadouelAlgorithm() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void containsTrianglePoint() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void intersectsCoplanarTriangle() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void intersectEdgeEdge() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void containsTrianglePoint3D() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void contains() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getNormal() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getPlane() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void clearBufferedData() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void toStringTest() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void distancePoint3D() {
+		throw new UnsupportedOperationException();
+	}
+	
+
+	@Test
+	public void distanceSquaredPoint3D() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void distanceL1Point3D() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void distanceLinfPoint3D() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getOrientation() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getPivot() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void transform() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void translate() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void rotate() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void rotatePoint3D() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void toBoundingBox() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void equals() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void containsProjectionOfPoint3D() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void isEmpty() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getClosestPointToPoint3D() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getFarthestPointToPoint3D() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getGroundHeight() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getPointOnGround() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getSegment1() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getSegment2() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	@Test
+	public void getSegment3() {
+		throw new UnsupportedOperationException();
+	}
+	
+}
