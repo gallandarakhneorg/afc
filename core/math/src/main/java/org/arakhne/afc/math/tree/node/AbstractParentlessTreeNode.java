b7dc7fcdf35ff1b66623069eba5bc08b4e7b3c73
Fevzi Ozgul <fevzi.ozgul@devfactory.com>
2016-06-06 20:15:35 +0300

    Fixing squid: S1948 Fields in a "Serializable" class should either be transient or serializable

--- core/math/src/main/java/org/arakhne/afc/math/tree/node/AbstractParentlessTreeNode.java
+++ core/math/src/main/java/org/arakhne/afc/math/tree/node/AbstractParentlessTreeNode.java
@@ -76,7 +76,7 @@ public abstract class AbstractParentlessTreeNode<D, N extends AbstractParentless
 
 	/** Associated user data.
 	 */
-	private List<D> data;
+	private transient List<D> data;
 
 	/** Indicates if a linked list must be used to store the data.
 	 * If <code>false</code>, an ArrayList will be used.
@@ -576,7 +576,7 @@ public abstract class AbstractParentlessTreeNode<D, N extends AbstractParentless
 
 		private static final long serialVersionUID = -8019401625554108057L;
 
-		private final List<D> backgroundList;
+		private final transient List<D> backgroundList;
 
 		/** Construct collection.
 		 * @param list the original list.
