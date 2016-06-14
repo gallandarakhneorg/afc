f7b76f3f7c719d99739e52b975db909eb1aaea96
Fevzi Ozgul <fevzi.ozgul@devfactory.com>
2016-06-14 10:15:16 +0300

    Adding final modifier to the  variable

--- core/math/src/main/java/org/arakhne/afc/math/geometry/d3/ai/Path3ai.java
+++ core/math/src/main/java/org/arakhne/afc/math/geometry/d3/ai/Path3ai.java
@@ -1599,7 +1599,7 @@ public interface Path3ai<
 		while (pi.hasNext()) {
 			pe = pi.next();
 
-			boolean foundCandidate;
+			final boolean foundCandidate;
 			final int candidateX;
 			final int candidateY;
 			final int candidateZ;
