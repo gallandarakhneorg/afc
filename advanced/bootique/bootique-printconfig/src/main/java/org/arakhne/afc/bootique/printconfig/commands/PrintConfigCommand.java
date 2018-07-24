4fb83d096bb7b58341bc148837c0e92b23dde6dd
Stéphane Galland <galland@arakhne.org>
2018-07-24 17:10:19 +0200

    [bootique-printconfig] Add output formater for Json and XML formats.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- advanced/bootique/bootique-printconfig/src/main/java/org/arakhne/afc/bootique/printconfig/commands/PrintConfigCommand.java
+++ advanced/bootique/bootique-printconfig/src/main/java/org/arakhne/afc/bootique/printconfig/commands/PrintConfigCommand.java
@@ -26,6 +26,7 @@ import java.util.TreeMap;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature;
 import com.google.inject.Injector;
@@ -35,8 +36,10 @@ import io.bootique.command.CommandOutcome;
 import io.bootique.command.CommandWithMetadata;
 import io.bootique.log.BootLogger;
 import io.bootique.meta.application.CommandMetadata;
+import io.bootique.meta.application.OptionMetadata;
 import io.bootique.meta.config.ConfigMetadataNode;
 import io.bootique.meta.module.ModulesMetadata;
+import joptsimple.OptionSpec;
 
 import org.arakhne.afc.bootique.printconfig.configs.Configs;
 import org.arakhne.afc.vmutil.locale.Locale;
@@ -56,6 +59,14 @@ public class PrintConfigCommand extends CommandWithMetadata {
 
 	private static final char CLI_SHORTNAME = 'C';
 
+	private static final String JSON_OPTION = "json"; //$NON-NLS-1$
+
+	private static final String XML_OPTION = "xml"; //$NON-NLS-1$
+
+	private static final String XML_ROOT_NAME = "configuration"; //$NON-NLS-1$
+
+	private static final int ERROR_CODE = 255;
+
 	private final Provider<BootLogger> bootLogger;
 
 	private final Provider<ModulesMetadata> modulesMetadata;
@@ -70,9 +81,17 @@ public class PrintConfigCommand extends CommandWithMetadata {
 	 */
 	public PrintConfigCommand(Provider<BootLogger> bootLogger, Provider<ModulesMetadata> modulesMetadata, Injector injector) {
 		super(CommandMetadata
-	            .builder(PrintConfigCommand.class)
-	            .description(Locale.getString("COMMAND_DESCRIPTION")) //$NON-NLS-1$
-	            .name(CLI_NAME).shortName(CLI_SHORTNAME));
+				.builder(PrintConfigCommand.class)
+				.description(Locale.getString("COMMAND_DESCRIPTION", JSON_OPTION, XML_OPTION)) //$NON-NLS-1$
+				.name(CLI_NAME).shortName(CLI_SHORTNAME)
+				.addOption(
+						OptionMetadata.builder(JSON_OPTION)
+						.description(Locale.getString("JSON_DESCRIPTION")) //$NON-NLS-1$
+						.build())
+				.addOption(
+						OptionMetadata.builder(XML_OPTION)
+						.description(Locale.getString("XML_DESCRIPTION")) //$NON-NLS-1$
+						.build()));
 		this.bootLogger = bootLogger;
 		this.modulesMetadata = modulesMetadata;
 		this.injector = injector;
@@ -82,10 +101,27 @@ public class PrintConfigCommand extends CommandWithMetadata {
 	public CommandOutcome run(Cli cli) {
 		final Map<String, Object> values = new TreeMap<>();
 		extractConfigValues(values, Configs.extractConfigs(this.modulesMetadata.get()));
+		// Search for the last format option
+		final List<OptionSpec<?>> options = cli.detectedOptions();
+		String lastOpt = null;
+		for (int i = options.size() - 1; lastOpt == null && i >= 0; --i) {
+			final OptionSpec<?> opt = options.get(i);
+			if (opt.options().contains(JSON_OPTION)) {
+				lastOpt = JSON_OPTION;
+			} else if (opt.options().contains(XML_OPTION)) {
+				lastOpt = XML_OPTION;
+			}
+		}
 		// Generate the output
 		final String content;
 		try {
+			if (lastOpt == JSON_OPTION) {
+				content = generateJson(values);
+			} else if (lastOpt == XML_OPTION) {
+				content = generateXml(values);
+			} else {
 				content = generateYaml(values);
+			}
 		} catch (JsonProcessingException exception) {
 			return CommandOutcome.failed(ERROR_CODE, exception.getLocalizedMessage(), exception);
 		}
@@ -118,6 +154,28 @@ public class PrintConfigCommand extends CommandWithMetadata {
 		return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
 	}
 
+	/** Generate the Json representation of the given map.
+	 *
+	 * @param map the map to print out.
+	 * @return the Json representation.
+	 * @throws JsonProcessingException when the Json cannot be processed.
+	 */
+	@SuppressWarnings("static-method")
+	protected String generateJson(Map<String, Object> map) throws JsonProcessingException {
+		final ObjectMapper mapper = new ObjectMapper();
+		return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+	}
+
+	/** Generate the Xml representation of the given map.
+	 *
+	 * @param map the map to print out.
+	 * @return the Xml representation.
+	 * @throws JsonProcessingException when XML cannot be processed.
+	 */
+	@SuppressWarnings({"static-method"})
+	protected String generateXml(Map<String, Object> map) throws JsonProcessingException {
+		final XmlMapper mapper = new XmlMapper();
+		return mapper.writerWithDefaultPrettyPrinter().withRootName(XML_ROOT_NAME).writeValueAsString(map);
 	}
 
 }
