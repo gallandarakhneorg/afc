35af38ce68634b2a9d9d26fe4accc3db9212ef0c
Stéphane Galland <galland@arakhne.org>
2022-08-02 02:42:49 +0200

    Change the dates of copyright in the file headers.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- advanced/slf4j/slf4j-maven/src/main/java/org/arakhne/afc/slf4j/maven/MavenSlf4jServiceProvider.java
+++ advanced/slf4j/slf4j-maven/src/main/java/org/arakhne/afc/slf4j/maven/MavenSlf4jServiceProvider.java
@@ -5,7 +5,7 @@
  * Copyright (c) 2000-2012 Stephane GALLAND.
  * Copyright (c) 2005-10, Multiagent Team, Laboratoire Systemes et Transports,
  *                        Universite de Technologie de Belfort-Montbeliard.
- * Copyright (c) 2013-2020 The original authors, and other authors.
+ * Copyright (c) 2013-2022 The original authors, and other authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,7 +66,7 @@ public class MavenSlf4jServiceProvider implements SLF4JServiceProvider {
 	}
 
 	@Override
-	public String getRequestedApiVersion() {
+	public String getRequesteApiVersion() {
 		return REQUESTED_API_VERSION;
 	}
 
